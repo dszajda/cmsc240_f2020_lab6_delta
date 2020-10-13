c863f9e6c1bbdfa009b585d0ec12348dd23fe8f5
an7zz <an7zz@mathcs03.richmond.edu>
2020-10-12 20:09:01 -0400

    Created implementation for IntegerVector.cpp

--- IntegerVector.cpp
+++ IntegerVector.cpp
@@ -4,43 +4,60 @@
 #include "CharacterVector.h"
 #include "DoubleVector.h"
 #include "IntegerVector.h"
+#include <vector>
 
 IntegerVector::IntegerVector()  {}
 IntegerVector::~IntegerVector() {}
 
 int IntegerVector::size() 
 {
-   return ...
+   return integerVector.size();
 }
 
 // just return the integer at the specified index; use the at() method rather
 // than [] so that an out_of_range error will be thrown for an invalid index
 int IntegerVector::get(int index)  
 { 
-   return ...
+   return integerVector.at(index);
 }
 
 // if index is size-legitimate, put the value at that index;
 // otherwise, use push_back to append to the end of the vector
 void IntegerVector::put(int value, int index)
 {
+   if (index >= 0 && index < integerVector.size()){
+      integerVector.at(index) = value;
+   }
+
+   else{
+      integerVector.push_back(value);
+   }
 }
 
 // use push_back to append
 void IntegerVector::put(int value)
 {
+   integerVector.push_back(value);
 }
 
 // for each char in characterVector, use static_cast<int> to append as an
 // integer to integerVector
 void IntegerVector::appendCharacterVector(CharacterVector& characterVector)
 {
+   for (int i = 0; i++; i < characterVector.size()){
+      int newInt = static_cast<int> (characterVector.get(i));
+      integerVector.push_back(newInt);
+   }
 }
 
 // for each double in doubleVector, use static_cast<int> to append as an
 // integer to integerVector
 void IntegerVector::appendDoubleVector(DoubleVector& doubleVector)
 {
+  for (int i = 0; i++; i < doubleVector.size()){
+      int newInt = static_cast<int> (doubleVector.get(i));
+      integerVector.push_back(newInt);
+   }
 }
 
 #endif
--- VectorTester.cpp
+++ VectorTester.cpp
@@ -21,7 +21,7 @@ int main()
    //-------------------------------------------------------------------------
 
    // test DoubleVector: put, get, size, out_of_range
-
+   
    std::cout << std::endl;
    std::cout << "-------------" << std::endl;
    std::cout << "DoubleVector:" << std::endl;
@@ -30,9 +30,20 @@ int main()
    //-------------------------------------------------------------------------
 
    // test IntegerVector: put, get, size, out_of_range
-
+ 
+   std::cout << std::endl;
    std::cout << "--------------" << std::endl;
    std::cout << "IntegerVector:" << std::endl;
+   std::cout << "Predicted: 0 " << "|| " << "Actual : " <<  iv.size() <<std::endl;
+   iv.put(30, 0);
+   iv.put(40, 1);
+   std::cout << "Predicted: 30 " << "|| " << "Actual : " <<  iv.get(0) <<std::endl;
+   iv.put(50, 2);
+   iv.put(120, 0);
+   std::cout << "Predicted: 120 " << "|| " << "Actual : " << iv.get(0) <<std::endl;
+   std::cout << "Predicted: 50 " << "|| " << "Actual : " <<  iv.get(2) <<std::endl;
+   std::cout << "Predicted: 3 " << "|| " << "Actual : " <<  iv.size() <<std::endl;
+   std::cout << "Predicted: out_of_range error " << "|| " << "Actual: " << iv.get(3) <<std::endl;
    std::cout << "--------------" << std::endl;
 
    //-------------------------------------------------------------------------
