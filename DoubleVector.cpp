b6a9d6b2a3c3ddada9b5108fd0a50016df767f62
dm9tb <dm9tb@mathcs05.richmond.edu>
2020-10-13 22:54:09 -0400

    Changes made to my DoubleVector.cpp functions and VectorTester.cpp was updated accordingly to test the functions

--- DoubleVector.cpp
+++ DoubleVector.cpp
@@ -5,42 +5,59 @@
 #include "DoubleVector.h"
 #include "IntegerVector.h"
 
+using namespace std;
+
 DoubleVector::DoubleVector()  {}
 DoubleVector::~DoubleVector() {}
 
 int DoubleVector::size() 
 {
-   return ...
+	return doubleVector.size();
 }
 
 // just return the double at the specified index; use the at() method rather
 // than [] so that an out_of_range error will be thrown for an invalid index
 double DoubleVector::get(int index) 
 { 
-   return ...
+	return doubleVector.at(index);
 }
 
 // if index is size-legitimate, put the value at that index;
 // otherwise, use push_back to append to the end of the vector
 void DoubleVector::put(double value, int index)
 {
+	if(index < 0 || index >= doubleVector.size()){
+		doubleVector.push_back(value);
+	}
+	else
+		value = doubleVector.at(index);
 }
 
 // use push_back to append
 void DoubleVector::put(double value)
 {
+	doubleVector.push_back(value);
 }
 
-// for each character in characteVector, use static_cast<double> to append as a
+// for each character in characterVector, use static_cast<double> to append as a
 // double to doubleVector
 void DoubleVector::appendCharacterVector(CharacterVector& characterVector)
 {
+	for(index = 0, index < characterVector.size(), index++){
+		double newDouble = static_cast<double>(characterVector.get(index));
+		doubleVector.push_back(newDouble);
+	}
 }
 
 // for each integer in integerVector, use static_cast<double> to append as a
 // double to doubleVector
 void DoubleVector::appendIntegerVector(IntegerVector& integerVector)
 {
+	for(index = 0, index < integerVector.size(), index++){
+		double newDouble = static_cast<double>(characterVector.get(index));
+		doubleVector.push_back(newDouble);
+	}
+		
 }
 
 #endif
--- VectorTester.cpp
+++ VectorTester.cpp
@@ -3,6 +3,8 @@
 #include "DoubleVector.h"
 #include "IntegerVector.h"
 
+using namespace std;
+
 int main()
 {
    CharacterVector cv;
@@ -26,6 +28,28 @@ int main()
    std::cout << "-------------" << std::endl;
    std::cout << "DoubleVector:" << std::endl;
    std::cout << "-------------" << std::endl;
+
+   //first test of the size() function
+   cout << "Predicted vector size: 0" << "||" << "Actual size: " << dv.size() << endl;
+
+   //testing the put and get index functions
+   dv.put(1.1, 0)
+   dv.put(1.2, 1)
+   dv.put(1.3, 2)
+   dv.put(1.4, 3)
+   cout << "Predicted value: 1.1 " << "||" << "Actual value: " << dv.get(0) << endl;
+   cout << "Predicted value: 1.2 " << "||" << "Actual value: " << dv.get(1) << endl;
+   cout << "Predicted value: 1.3 " << "||" << "Actual value: " << dv.get(2) << end1;
+   cout << "predicted value: 1.4 " << "||" << "Actual value: " << dv.get(3) << endl;
+
+   //testing the size() function again
+   cout << "Predicted vector size: 4" << "||" << "Actual size: " << dv.size() << end1;
+   
+   //testing out_of_range
+   cout << "Predicted: out_of_range error " << "||" << "Actual error: " << dv.get(4) << end1;
+   cout << "Predicted: out_of_range error " << "||" << "Actual error: " << dv.get(-1) << end1;
+   cout << "-------------" << endl;
+
 
    //-------------------------------------------------------------------------
 
